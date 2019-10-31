8e43d946c07e634234ba98ddcd4097e791e49e85
brendan-c <brendan.k.cooney@gmail.com>
2019-10-31 08:53:35 -0400

    utilized event object

--- src/javascript.js
+++ src/javascript.js
@@ -1,58 +1,79 @@
-
-window.onload = init;
-
-function init() {
-    document.getElementById("thetext").onclick = function () { toggleEditor("thetext", "ta1", "editor");}
-    document.getElementById("thetextHome").onclick = function () { toggleEditor("thetextHome", "ta2", "editorHome");}
-    document.getElementById("thetextVisitor").onclick = function () { toggleEditor("thetextVisitor", "ta3", "editorVisitor");}
-
-
-    document.getElementById("submit").onclick = function () { doEdit("thetext", "ta1", "editor");}
-    document.getElementById("submit2").onclick = function () { doEdit("thetextHome", "ta2", "editorHome");}
-    document.getElementById("submit3").onclick = function () { doEdit("thetextVisitor", "ta3", "editorVisitor");}
-
-    document.getElementById("subHome").onclick = function () { counter(-1, "scoreHome"); }
-    document.getElementById("addHome").onclick = function () { counter(1, "scoreHome"); }
-    document.getElementById("subVisitor").onclick = function () { counter(-1, "scoreVisitor"); }
-    document.getElementById("addVisitor").onclick = function () { counter(1, "scoreVisitor"); }
-}
-
-
-function counter(num, target) {
-	var score = document.getElementById(target);
-  score.innerHTML = parseInt(score.textContent) + num;
-}
-
-
-function toggleEditor(textid, inputid, editorid) {
-	var theText = document.getElementById(textid);
-  var theEditor = document.getElementById(inputid);
-	var editorArea = document.getElementById(editorid);
-
-	var subject = theText.innerHTML;
-	theEditor.value = subject;
-
-	theText.style.display = 'none';
-	editorArea.style.display = 'inline';
-}
-
-
-function doEdit(textid, inputid, editorid) {
-	var theText = document.getElementById(textid);
-	var theEditor = document.getElementById(inputid);
-	var editorArea = document.getElementById(editorid);
-
-	var subject = theEditor.value;
-  subject = sanitize(subject);
-  theText.innerHTML = subject;
-
-	theText.style.display = 'inline';
-	editorArea.style.display = 'none';
-}
-
-
 function sanitize(message) {
   message = message.replace(new RegExp("<", "g"), ' ');
   message = message.replace(new RegExp(">", "g"), ' ');
   return message
 }
+
+
+function toggleEditor(event) {
+  const container = event.target.parentElement.nodeName === 'H1' // if it's in the h1 title
+    ?
+    event.target.parentNode // the container is the h1
+    :
+    event.target.closest('.column') // else its a column
+
+  const text = container.querySelector('.title')
+  const input = container.querySelector('input[type="text"]')
+  const editor = container.querySelector('.editor')
+
+  let subject = text.innerHTML;
+  input.value = subject;
+
+  text.style.display = 'none';
+  editor.style.display = 'inline';
+}
+
+
+function doEdit(event) {
+  const container = event.target.parentElement.nodeName === 'H1' // if it's in the h1 title
+    ?
+    event.target.parentNode // the container is the h1
+    :
+    event.target.closest('.column') // else its a column
+
+  const text = container.querySelector('.title')
+  const input = container.querySelector('input[type="text"]')
+  const editor = container.querySelector('.editor')
+
+  var subject = input.value;
+  subject = sanitize(subject);
+  text.innerHTML = subject;
+
+  text.style.display = 'inline';
+  editor.style.display = 'none';
+}
+
+function changeScore(event) {
+  const column = event.target.closest('.column')
+  const btn = event.target
+  const action = btn.classList.contains('add') ? 'add' : 'sub'
+  const scoreElement = column.querySelector('.score')
+  let scoreValue = scoreElement.textContent
+  if (action === 'add') {
+    scoreElement.innerText = Number(scoreValue) + 1
+  } else {
+    scoreElement.innerText = Number(scoreValue) - 1
+  }
+}
+
+
+// determine what sort button was clicked
+// redirect to the appropiate function
+function handleButtonClick(event) {
+  const clickedButton = event.target.classList[0]
+  if (clickedButton === 'change_score') {
+    changeScore(event)
+  } else if (clickedButton === 'set_title') {
+    doEdit(event)
+  } else if (clickedButton === 'title') {
+    toggleEditor(event)
+  }
+}
+
+//Get collection of buttons
+const buttons = document.querySelectorAll('.btn')
+buttons.forEach(button => {
+  // add a click event listener to each button
+  // and run the handleButtonClick function
+  button.addEventListener('click', handleButtonClick)
+})
